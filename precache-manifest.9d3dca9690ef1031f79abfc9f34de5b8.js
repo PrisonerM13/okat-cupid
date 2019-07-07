aa53b2665293ca165f255774cb4a265e7fc236c8
PrisonerM13 <PrisonerM13@outlook.com>
2019-07-07 20:24:18 +0300

    Updates

---
+++ precache-manifest.9d3dca9690ef1031f79abfc9f34de5b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682aee5f5b1cba989ea3ce4e8486a951",
+    "url": "/okat-cupid/index.html"
+  },
+  {
+    "revision": "b9c2d46cad8a7c7227e6",
+    "url": "/okat-cupid/static/css/main.82786d5b.chunk.css"
+  },
+  {
+    "revision": "3730b3db477ca89fea59",
+    "url": "/okat-cupid/static/js/2.2f91856e.chunk.js"
+  },
+  {
+    "revision": "b9c2d46cad8a7c7227e6",
+    "url": "/okat-cupid/static/js/main.c9cbb3fd.chunk.js"
+  },
+  {
+    "revision": "ac4d2b7ca81d0f804757",
+    "url": "/okat-cupid/static/js/runtime~main.2982c164.js"
+  }
+]);
